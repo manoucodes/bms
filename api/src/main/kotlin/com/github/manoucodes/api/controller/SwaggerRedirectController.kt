8d8548f925cf4c9f11fc2c63733d24c3ce997ab0
Manou <emmanuelbenoitsarr@gmail.com>
2025-02-27 23:57:07 -0500

    refactor from deepsource comments

--- api/src/main/kotlin/com/github/manoucodes/api/controller/SwaggerRedirectController.kt
+++ api/src/main/kotlin/com/github/manoucodes/api/controller/SwaggerRedirectController.kt
@@ -3,11 +3,13 @@ package com.github.manoucodes.api.controller
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
+const val swaggerEndpoint = "/swagger-ui.html"
+
 @Controller
 class SwaggerRedirectController {
 
     @GetMapping("/")
     fun redirectToSwagger(): String {
-        return "redirect:/swagger-ui.html"
+        return "redirect:$swaggerEndpoint"
     }
 }
