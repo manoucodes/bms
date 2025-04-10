83b2b7e26921d305dfb1ebb490d52b92fb86831f
Manou <emmanuelbenoitsarr@gmail.com>
2025-04-10 12:48:53 -0400

    allows cors for staging env

---
+++ api-client/src/main/kotlin/com/github/manoucodes/api/config/CorsConfig.kt
@@ -0,0 +1,22 @@
+package com.github.manoucodes.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Profile("staging")
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer =
+        object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("*") // Allow all origins (for now)
+                    .allowedMethods("*") // Allow GET, POST, PUT, DELETE, etc.
+            }
+        }
+}
