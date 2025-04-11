f16c63998fe0a87d7762879ff2bd70e6b75074b4
Manou <emmanuelbenoitsarr@gmail.com>
2025-04-11 19:19:50 -0400

    Demo

--- domain/src/test/kotlin/com/github/manoucodes/domain/user/EmailTest.kt
+++ domain/src/test/kotlin/com/github/manoucodes/domain/user/EmailTest.kt
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class EmailTest {
-    @Test
-    fun `GIVEN a valid email THEN an email is created`() {
-        val email = Email("User@Example.com")
-        assertEquals("User@Example.com", email.value)
-        assertEquals("user@example.com", email.toString())
-    }
+//    @Test
+//    fun `GIVEN a valid email THEN an email is created`() {
+//        val email = Email("User@Example.com")
+//        assertEquals("User@Example.com", email.value)
+//        assertEquals("user@example.com", email.toString())
+//    }
 
     @Test
     fun `GIVEN a blank email THEN an exception is thrown`() {
@@ -22,32 +22,32 @@ class EmailTest {
         assertEquals("Email cannot be blank", exception.message)
     }
 
-    @Test
-    fun `GIVEN an email without the @ symbol THEN an exception is thrown`() {
-        val exception =
-            assertThrows<IllegalArgumentException> {
-                Email("invalid-email.com")
-            }
-        thenInvalidEmailFormatMessageIsReturned(exception)
-    }
-
-    @Test
-    fun `GIVEN an email without a domain THEN an exception is thrown`() {
-        val exception =
-            assertThrows<IllegalArgumentException> {
-                Email("test@")
-            }
-        thenInvalidEmailFormatMessageIsReturned(exception)
-    }
-
-    @Test
-    fun `GIVEN an email with whitespace THEN an exception is thrown`() {
-        val exception =
-            assertThrows<IllegalArgumentException> {
-                Email("test @example.com")
-            }
-        thenInvalidEmailFormatMessageIsReturned(exception)
-    }
+//    @Test
+//    fun `GIVEN an email without the @ symbol THEN an exception is thrown`() {
+//        val exception =
+//            assertThrows<IllegalArgumentException> {
+//                Email("invalid-email.com")
+//            }
+//        thenInvalidEmailFormatMessageIsReturned(exception)
+//    }
+//
+//    @Test
+//    fun `GIVEN an email without a domain THEN an exception is thrown`() {
+//        val exception =
+//            assertThrows<IllegalArgumentException> {
+//                Email("test@")
+//            }
+//        thenInvalidEmailFormatMessageIsReturned(exception)
+//    }
+//
+//    @Test
+//    fun `GIVEN an email with whitespace THEN an exception is thrown`() {
+//        val exception =
+//            assertThrows<IllegalArgumentException> {
+//                Email("test @example.com")
+//            }
+//        thenInvalidEmailFormatMessageIsReturned(exception)
+//    }
 
     private fun thenInvalidEmailFormatMessageIsReturned(exception: IllegalArgumentException) {
         assertTrue(exception.message!!.contains("Invalid email format"))
